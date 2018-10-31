53511c926c1f1d0e231f3290cab47c5e26add8ae
David <davidespinoherrera@gmail.com>
2018-10-31 13:12:43 +0100

    Initial commit

---
+++ Assets/PortadaScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortadaScript : MonoBehaviour {
+
+    [SerializeField] RectTransform[] rt;
+    [SerializeField] float speed = 1f;
+
+    private void Update() {
+
+        float xPos = -1 * Time.deltaTime * speed;
+        for (int i = 0; i < rt.Length; i++) {
+            if ((rt[i].position.x + rt[i].rect.width) < 0) {
+                xPos = 1000;
+            }
+            rt[i].Translate(xPos, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    public void StartGame() {
+
+        SceneManager.LoadScene(1);
+    }
+
+
+}
---
+++ Assets/_GameAssests/Scripts/MarcianoScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MarcianoScript : MonoBehaviour {
+
+    [SerializeField] int vidas;
+    [SerializeField] int puntuacion;
+    [SerializeField] LayerMask floorLayer;
+    [SerializeField] Transform posPies;
+    [SerializeField] Text txtPuntuacion;
+    [SerializeField] float speed = 5;
+    [SerializeField] float jumpForce = 10;
+    [SerializeField] float radioOverlap = 0.1f;
+    Rigidbody2D rb2D;
+    int vidasMaximas = 5;
+    bool saltando = true;
+    bool enSuelo;
+
+
+
+    public void incrementarPuntuacion(int puntuacion) {
+
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+        txtPuntuacion.text = "Score: " + puntuacion;
+
+
+    }
+
+    private bool EstaEnSuelo() {
+        bool enSuelo = false;
+        Collider2D col = Physics2D.OverlapCircle(posPies.position, radioOverlap, floorLayer);
+        if (col != null) {
+
+            enSuelo = true;
+        }
+
+        return enSuelo;
+    }
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            saltando = true;
+        }
+
+    }
+    private void FixedUpdate() {
+
+        float xPos=Input.GetAxis("Vertical");
+        float yPos=Input.GetAxis("Horizontal");
+        float ySpeedActual = rb2D.velocity.y;
+
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (EstaEnSuelo()) {
+                rb2D.velocity = new Vector3(xPos * speed, jumpForce);
+            }
+
+        }
+    }
+
+    
+}
